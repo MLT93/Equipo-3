7014322dc8720eba6409f733cff6cb8e64913120
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-06-22 17:52:41 +0200

    añado objeto al array en el archivo team-members.js

--- team-members.js
+++ team-members.js
@@ -1,4 +1,15 @@
 
 let teamMembers = [
-    
-]
+    {
+    namee: "Marcos",
+    surname: "Lambir Torres",
+    age: 30,
+    city: "Franchuski",
+    hobby: "Rascarme",
+    favoriteFood: "Pasta",
+    favoriteVideoGame: "Lol",
+    favoriteFilm: "Titanic",
+    favoriteBook: "Memorias del Agua",
+    petName: "Turi",
+    },
+];
