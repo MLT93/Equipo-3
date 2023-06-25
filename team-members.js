1705bc480e373a93a72ed776ea4ad804ac770901
Marta Miras <martamirasmendez@gmail.com>
2023-06-25 22:21:38 +0200

    feat: Agrego mis datos al archivo .js

--- team-members.js
+++ team-members.js
@@ -1,4 +1,14 @@
-
 let teamMembers = [
-    
-]
+  {
+    name: "Marta",
+    surname: "Miras Méndez",
+    age: 21,
+    city: "Madrid",
+    hobby: "Papiroflexia",
+    favoriteFood: "Sushi",
+    favoriteVideoGame: "PUBG",
+    favoriteFilm: "Avatar",
+    favoriteBook: "Loco por ella",
+    petName: "Mochi",
+  },
+];
