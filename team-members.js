3fd2efff6bb8a18bf0db94c7c70a608578b53f9e
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-06-26 14:59:25 +0200

    feat(team-members.js): agrego código JavaScript del ejercicio que mandó Manuel

--- team-members.js
+++ team-members.js
@@ -19,7 +19,7 @@ let teamMembers = [
     city: "Granollers",
     hobby: "Escalar",
     favoriteFood: "Croquetas",
-    favoriteVideoGame: "Lol",
+    favoriteVideoGame: "LOL",
     favoriteFilm: "Origen",
     favoriteBook: "",
     petName: "",
@@ -50,28 +50,93 @@ let teamMembers = [
   },
 ];
 
+
+teamMembers.sort((a, b) => {
+  /* .toLowerCase convierte todas las letras a minúsculas. De éste modo, el órden que pretendemos ejecutar se realiza correctamente. */
+    let fa = a.surname.toLowerCase(),
+        fb = b.surname.toLowerCase();
+
+    if (fa < fb) {
+        return -1;
+    }
+
+    if (fa > fb) {
+      return 1;
+    } 
+
+    return 0;
+});
+
+teamMembers.forEach(element => {
+return console.log(`${element.surname} ${element.name} `)
+});
+
+/* El .sort nos compara los elementos dentro de un array y nos devuelve el mismo arreglo ordenado, después de hacer una comparación entre el elemento "a" y el elemento "b" */
+teamMembers.sort((a, b) => {
+  return a.age - b.age;
+});
+
+teamMembers.forEach(element => {
+  return console.log(`La edad de ${element.name} es ${element.age} `)
+});
+
+let longitudDelArray = teamMembers.length;
+let sumaEdades = 0;
+
+teamMembers.forEach(lasEdades => {
+  sumaEdades += lasEdades.age
+});
+
+console.log("La edad media es:", sumaEdades / longitudDelArray);
+
+teamMembers.forEach(element => {
+  let juegoFavorito = element.favoriteVideoGame
+  if (
+    juegoFavorito.toLowerCase() === "lol" || juegoFavorito.toLowerCase() === "league of legends"
+    ) {
+      console.log(`${element.name} es un fanático de League Of Legends`);
+  };
+});
+
 function recorrerElArray(arr) {
   for (let i = 0; i < arr.length; i++) {
     let indiceDelParticipante = i;
-    let saberLibroFavorito = arr[i].favoriteBook;
-    let esMayorDeEdad = arr[i].age;
+    let saberPetName = arr[i].petName;
 
     if (
-      typeof arr[i].favoriteBook !== "string" ||
-      arr[i].favoriteBook.length === 0
+      typeof saberPetName === "string" &&
+      saberPetName.length !== 0
     ) {
-      saberLibroFavorito = "N/A";
+      console.log(`Nombre de la/las mascotas que tiene ${arr[i].name}: ${saberPetName}`);
+    } else {
+      console.log(saberPetName = `${arr[i].name} no tiene mascota y vive triste`);
     }
 
-    if (typeof arr[i].age !== "number" || arr[i].age <= 17) {
-      esMayorDeEdad = "No es mayor de edad";
+
+    switch (arr[i].favoriteVideoGame) {
+      case "LoL":
+        
+        break;
+      case "League of Legends":
+        
+        break;
+    
+      default:
+        break;
     }
 
-    console.log(`El indice de este participante es: ${indiceDelParticipante}`);
-    console.log(`La edad que tiene ${arr[i].name} es: ${esMayorDeEdad}`);
-    console.log(
-      `El libro favorito de ${arr[i].name} es: ${saberLibroFavorito}`
-    );
+
+
+
+    //if (typeof arr[i].age !== "number" || arr[i].age <= 17) {
+    //  esMayorDeEdad = "No es mayor de edad";
+    //}
+
+        
+
+    //console.log(`El indice de este participante es: ${indiceDelParticipante}`);
+    //console.log(`El libro favorito de ${arr[i].name} es: ${petName}`
+    //);
   }
 }
 
